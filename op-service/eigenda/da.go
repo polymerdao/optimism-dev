2519b568d48755793b24fa742b742a5b8d0e38ae
Derek <derek@polymerlabs.org>
2024-04-11 14:28:53 -0700

    update eigenda blob retrieve/disperse function for new eigenda client

--- op-service/eigenda/da.go
+++ op-service/eigenda/da.go
@@ -6,10 +6,12 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
-	"google.golang.org/grpc/credentials"
 	"time"
 
+	"google.golang.org/grpc/credentials"
+
 	"github.com/Layr-Labs/eigenda/api/grpc/disperser"
+	"github.com/Layr-Labs/eigenda/encoding/utils/codec"
 	"github.com/ethereum/go-ethereum/log"
 	"google.golang.org/grpc"
 )
@@ -26,7 +28,10 @@ type EigenDA struct {
 }
 
 func (m *EigenDA) RetrieveBlob(ctx context.Context, BatchHeaderHash []byte, BlobIndex uint32) ([]byte, error) {
-	conn, err := m.GetConn()
+	config := &tls.Config{}
+	credential := credentials.NewTLS(config)
+	dialOptions := []grpc.DialOption{grpc.WithTransportCredentials(credential)}
+	conn, err := grpc.Dial(m.RPC, dialOptions...)
 	if err != nil {
 		return nil, err
 	}
@@ -39,20 +44,29 @@ func (m *EigenDA) RetrieveBlob(ctx context.Context, BatchHeaderHash []byte, Blob
 	if err != nil {
 		return nil, err
 	}
-	return reply.Data, nil
+
+	// decode modulo bn254
+	decodedData := codec.RemoveEmptyByteFromPaddedBytes(reply.Data)
+
+	return decodedData, nil
 }
 
 func (m *EigenDA) DisperseBlob(ctx context.Context, txData []byte) (*disperser.BlobInfo, error) {
-	conn, err := m.GetConn()
+	m.Log.Info("Attempting to disperse blob to EigenDA")
+	config := &tls.Config{}
+	credential := credentials.NewTLS(config)
+	dialOptions := []grpc.DialOption{grpc.WithTransportCredentials(credential)}
+	conn, err := grpc.Dial(m.RPC, dialOptions...)
 	if err != nil {
-		m.Log.Error("Unable to connect to EigenDA, aborting")
 		return nil, err
 	}
 	daClient := disperser.NewDisperserClient(conn)
 
+	// encode modulo bn254
+	encodedTxData := codec.ConvertByPaddingEmptyByte(txData)
+
 	disperseReq := &disperser.DisperseBlobRequest{
-		Data:           txData,
-		SecurityParams: m.DisperserSecurityParams,
+		Data: encodedTxData,
 	}
 	disperseRes, err := daClient.DisperseBlob(ctx, disperseReq)
 
@@ -81,7 +95,7 @@ func (m *EigenDA) DisperseBlob(ctx context.Context, txData []byte) (*disperser.B
 		})
 		if err != nil {
 			m.Log.Warn("Unable to retrieve blob dispersal status, will retry", "requestID", base64RequestID, "err", err)
-		} else if statusRes.Status == disperser.BlobStatus_CONFIRMED {
+		} else if statusRes.Status == disperser.BlobStatus_CONFIRMED || statusRes.Status == disperser.BlobStatus_FINALIZED {
 			// TODO(eigenlayer): As long as fault proofs are disabled, we can move on once a blob is confirmed
 			// but not yet finalized, without further logic. Once fault proofs are enabled, we will need to update
 			// the proposer to wait until the blob associated with an L2 block has been finalized, i.e. the EigenDA
@@ -102,12 +116,4 @@ func (m *EigenDA) DisperseBlob(ctx context.Context, txData []byte) (*disperser.B
 	}
 
 	return nil, fmt.Errorf("timed out getting EigenDA status for dispersed blob key: %s", base64RequestID)
-}
-
-func (m *EigenDA) GetConn() (*grpc.ClientConn, error) {
-	config := &tls.Config{}
-	credential := credentials.NewTLS(config)
-	options := []grpc.DialOption{grpc.WithTransportCredentials(credential)}
-	conn, err := grpc.Dial(m.RPC, options[0])
-	return conn, err
 }
