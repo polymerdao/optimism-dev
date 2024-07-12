c91efebed3d132fc6ed51c0378f6c7e756aee0d2
i-norden <iansnorden@gmail.com>
2024-07-12 17:17:27 -0400

    op-dachallenger config

---
+++ op-dachallenger/config/config.go
@@ -0,0 +1,130 @@
+package config
+
+import (
+	"errors"
+	plasma "github.com/ethereum-optimism/optimism/op-plasma"
+	"runtime"
+	"time"
+
+	opmetrics "github.com/ethereum-optimism/optimism/op-service/metrics"
+	"github.com/ethereum-optimism/optimism/op-service/oppprof"
+	"github.com/ethereum-optimism/optimism/op-service/txmgr"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var (
+	ErrMissingDatadir                   = errors.New("missing datadir")
+	ErrMaxConcurrencyZero               = errors.New("max concurrency must not be 0")
+	ErrMissingL1EthRPC                  = errors.New("missing l1 eth rpc url")
+	ErrMissingL1Beacon                  = errors.New("missing l1 beacon url")
+	ErrMissingPlasmaServerRPC        	= errors.New("missing op plasma da server url")
+	ErrMissingBatchInboxAddress 		= errors.New("missing batch inbox address")
+	ErrMissingDAChallengeAddress 		= errors.New("missing da challenge contract address")
+	ErrInvalidDACommitmentType 		    = errors.New("invalid da challenge type")
+	ErrMissingDACommitmentKind 		    = errors.New("missing generic da challenge kind")
+)
+
+const (
+	DefaultPollInterval         = time.Second * 12
+	DefaultMaxPendingTx = 10
+)
+
+// Config is a well typed config that is parsed from the CLI params.
+// This also contains config options for auxiliary services.
+// It is used to initialize the challenger.
+type Config struct {
+	L1EthRpc             string           // L1 RPC Url
+	L1Beacon             string           // L1 Beacon API Url
+	PlasmaServerRpc      string			  // Plasma DA server Url
+	BatchInboxAddress    common.Address   // Address of the dispute game factory
+	DAChallengeAddress   common.Address
+	BatcherAddresses     []common.Address // Allowed batch submitters, ignore all other
+	Datadir              string           // Data Directory
+	MaxConcurrency       uint             // Maximum number of threads to use when progressing games
+	PollInterval         time.Duration    // Polling interval for latest-block subscription when using an HTTP RPC provider
+
+	MaxPendingTx uint64 // Maximum number of pending transactions (0 == no limit)
+
+	TxMgrConfig   txmgr.CLIConfig
+	MetricsConfig opmetrics.CLIConfig
+	PprofConfig   oppprof.CLIConfig
+
+	CommitmentType plasma.CommitmentType
+	CommitmentKind string
+}
+
+func NewConfig(
+	batchInboxAddress common.Address,
+	daChallengeAddress common.Address,
+	l1EthRpc string,
+	l1BeaconApi string,
+	plasmaRPC string,
+	datadir string,
+	batcherAddrs []common.Address,
+	commitmentType plasma.CommitmentType,
+	commitmentKind string,
+) Config {
+	return Config{
+		L1EthRpc:           l1EthRpc,
+		L1Beacon:           l1BeaconApi,
+		PlasmaServerRpc:    plasmaRPC,
+		BatchInboxAddress:  batchInboxAddress,
+		BatcherAddresses:   batcherAddrs,
+		DAChallengeAddress: daChallengeAddress,
+		MaxConcurrency:     uint(runtime.NumCPU()),
+		PollInterval:       DefaultPollInterval,
+
+		MaxPendingTx: DefaultMaxPendingTx,
+
+		TxMgrConfig:   txmgr.NewCLIConfig(l1EthRpc, txmgr.DefaultChallengerFlagValues),
+		MetricsConfig: opmetrics.DefaultCLIConfig(),
+		PprofConfig:   oppprof.DefaultCLIConfig(),
+
+		Datadir: datadir,
+
+		CommitmentType: commitmentType,
+		CommitmentKind: commitmentKind,
+	}
+}
+
+func (c Config) Check() error {
+	if c.L1EthRpc == "" {
+		return ErrMissingL1EthRPC
+	}
+	if c.L1Beacon == "" {
+		return ErrMissingL1Beacon
+	}
+	if c.PlasmaServerRpc == "" {
+		return ErrMissingPlasmaServerRPC
+	}
+	if c.BatchInboxAddress == (common.Address{}) {
+		return ErrMissingBatchInboxAddress
+	}
+	if c.DAChallengeAddress == (common.Address{}) {
+		return ErrMissingDAChallengeAddress
+	}
+	if c.CommitmentType > 1 {
+		return ErrInvalidDACommitmentType
+	}
+	if c.CommitmentType == plasma.GenericCommitmentType {
+		if c.CommitmentKind == "" {
+			return ErrMissingDACommitmentKind
+		}
+	}
+	if c.Datadir == "" {
+		return ErrMissingDatadir
+	}
+	if c.MaxConcurrency == 0 {
+		return ErrMaxConcurrencyZero
+	}
+	if err := c.TxMgrConfig.Check(); err != nil {
+		return err
+	}
+	if err := c.MetricsConfig.Check(); err != nil {
+		return err
+	}
+	if err := c.PprofConfig.Check(); err != nil {
+		return err
+	}
+	return nil
+}
