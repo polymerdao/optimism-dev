b7cfcea44525d333edae024fce3d734096723b4f
RnkSngh <raunaksingh2.718@gmail.com>
2024-08-28 21:51:48 -0400

    update deploy script to read from config

--- packages/contracts-bedrock/deployPeptideL1Contracts.sh
+++ packages/contracts-bedrock/deployPeptideL1Contracts.sh
@@ -1,20 +1,21 @@
+set -ueo pipefail
 
-export OLD_DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet.json
-export DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet-1.json
-export IMPL_SALT="polymer-deploy-1"
+export IMPL_SALT="polymer-deploy-2"
+export DEPLOY_CONFIG_PATH=${BASE_DEPLOY_CONFIG}-1.json # Note: BASE_DEPLOY_CONFIG should be without the .json extension
 
-cat $DEPLOY_CONFIG_PATH
-
-export blockNumber=$(cast to-dec $(cast block --rpc-url $LOCAL_RPC -j | jq -r .number))
+export blockNumber=$(cast to-dec $(cast block --rpc-url $RPC_URL -j | jq -r .number))
 
 echo $blockNumber
 # Deploy l2output oracle address
-jq --arg new_value $blockNumber '.l2OutputOracleStartingBlockNumber= $new_value' $OLD_DEPLOY_CONFIG_PATH > $DEPLOY_CONFIG_PATH
+jq --arg new_value $blockNumber '.l2OutputOracleStartingBlockNumber= $new_value' ${BASE_DEPLOY_CONFIG}.json > $DEPLOY_CONFIG_PATH
+
+echo "using base deploy config: "
+cat $DEPLOY_CONFIG_PATH
 
 forge script \
     scripts/Deploy.s.sol:Deploy \
     --sig runPolymerContractsWithStateDump \
     --broadcast \
-    --private-key $DUMMY_PRIVATE_KEY \
-    --rpc-url $LOCAL_RPC
+    --private-key $DEPLOYER_PRIVATE_KEY \
+    --rpc-url $RPC_URL
 
