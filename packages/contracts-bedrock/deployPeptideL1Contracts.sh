20e9778a7da751ef364d19751be3e94e4cf0a440
RnkSngh <raunaksingh2.718@gmail.com>
2024-08-28 21:51:48 -0400

    update deploy script to confirm from user

--- packages/contracts-bedrock/deployPeptideL1Contracts.sh
+++ packages/contracts-bedrock/deployPeptideL1Contracts.sh
@@ -1,20 +1,36 @@
+set -ueo pipefail
 
-export OLD_DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet.json
-export DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet-1.json
-export IMPL_SALT="polymer-deploy-1"
+export IMPL_SALT="polymer-deploy-10"
+export DEPLOY_CONFIG_PATH=$(readlink -f ${BASE_DEPLOY_CONFIG}.json) # Note: BASE_DEPLOY_CONFIG should be without the .json extension
 
+
+export CHAIN_ID=$(cast chain-id --rpc-url $RPC_URL)
+echo "Starting opstack deploy on chainId: $CHAIN_ID"
+read -p  "Is this correct? hit enter to continue..."
+echo "Using RPC: $RPC_URL"
+read -p  "Is this correct? hit enter to continue..."
+
+export blockNumber=$(cast to-dec $(cast block --rpc-url $RPC_URL -j | jq -r .number))
+
+echo "Using block l2OO start block number $blockNumber "
+read -p  "Is this correct? hit enter to continue..."
+
+# Deploy l2output oracle address
+
+echo "this is the config we will use to deploy: "
 cat $DEPLOY_CONFIG_PATH
 
-export blockNumber=$(cast to-dec $(cast block --rpc-url $LOCAL_RPC -j | jq -r .number))
+echo "from path $DEPLOY_CONFIG_PATH"
+read -p "Does this look correct? Hit enter to continue..."
 
-echo $blockNumber
-# Deploy l2output oracle address
-jq --arg new_value $blockNumber '.l2OutputOracleStartingBlockNumber= $new_value' $OLD_DEPLOY_CONFIG_PATH > $DEPLOY_CONFIG_PATH
+echo "from deployer address $(cast wallet address --private-key $DEPLOYER_PRIVATE_KEY)"
+read -p "Does this look correct? Hit enter to continue..."
 
 forge script \
     scripts/Deploy.s.sol:Deploy \
     --sig runPolymerContractsWithStateDump \
     --broadcast \
-    --private-key $DUMMY_PRIVATE_KEY \
-    --rpc-url $LOCAL_RPC
+    --private-key $DEPLOYER_PRIVATE_KEY \
+    --rpc-url $RPC_URL \
+    --slow
 
