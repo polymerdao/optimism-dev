cd772cd921fef20c184f3e03f0387be363b68066
wenwei <wenwei@polymer>
2024-11-13 15:55:37 -0500

    feat: add cli arg for L1Tracker cache size with default 2000
    default 2000 L1 headers is around 6.67 hr

--- op-node/flags/flags.go
+++ op-node/flags/flags.go
@@ -256,6 +256,13 @@ var (
 		Value:    time.Second * 12 * 32,
 		Category: L1RPCCategory,
 	}
+	L1TrackerCacheFlag = &cli.IntFlag{
+		Name:     "l1.tracker-cache",
+		Usage:    "Number of L1BlockRefs to cache in L1 Tracker.",
+		EnvVars:  prefixEnvVars("L1_TRACKER_CACHE"),
+		Value:    2000,
+		Category: L1RPCCategory,
+	}
 	RuntimeConfigReloadIntervalFlag = &cli.DurationFlag{
 		Name:     "l1.runtime-config-reload-interval",
 		Usage:    "Poll interval for reloading the runtime config, useful when config events are not being picked up. Disabled if 0 or negative.",
@@ -449,6 +456,7 @@ var optionalFlags = []cli.Flag{
 	SequencerMaxSafeLagFlag,
 	SequencerL1Confs,
 	L1EpochPollIntervalFlag,
+	L1TrackerCacheFlag,
 	RuntimeConfigReloadIntervalFlag,
 	RPCEnableAdmin,
 	RPCAdminPersistence,
