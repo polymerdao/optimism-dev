2b5198a4e685102bc79920e4fa4c933bc118c77a
Derek <derek@polymerlabs.org>
2024-12-11 18:29:23 -0800

    set MaxInputSize to 300Mb

--- op-alt-da/params.go
+++ op-alt-da/params.go
@@ -3,4 +3,4 @@ package altda
 // MaxInputSize ensures the canonical chain cannot include input batches too large to
 // challenge in the Data Availability Challenge contract. Value in number of bytes.
 // This value can only be changed in a hard fork.
-const MaxInputSize = 130672
+const MaxInputSize = 300_000_000
