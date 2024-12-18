43e0022b560239f6363340d404733ad83794afa3
wenwei_0x <77592522+alfredo-stonk@users.noreply.github.com>
2024-12-18 09:15:56 -0500

    feat: update frame validity check to account for altDA (#23)
    Co-authored-by: wenwei <wenwei@polymer>

--- op-node/node/config.go
+++ op-node/node/config.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/flags"
 	"github.com/ethereum-optimism/optimism/op-node/p2p"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/driver"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/sync"
 	"github.com/ethereum-optimism/optimism/op-service/oppprof"
@@ -182,6 +183,10 @@ func (cfg *Config) Check() error {
 	}
 	if cfg.AltDA.Enabled {
 		log.Warn("Alt-DA Mode is a Beta feature of the MIT licensed OP Stack.  While it has received initial review from core contributors, it is still undergoing testing, and may have bugs or other issues.")
+
+		// Update the max frame size for altDA mode; otherwise channel frames from altDA are likely to exceed the
+		// default max frame size (1MB) and thus get discarded by op-node during block derivation.
+		derive.SetMaxFrameLenForAllDA(altda.MaxInputSize)
 	}
 	return nil
 }
