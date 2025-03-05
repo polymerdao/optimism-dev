51e930deda4fdbf05e51524a6aeb34e27d79e11f
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2025-03-05 16:06:22 +0100

    add pectra support (kindof)

--- op-service/eth/types.go
+++ op-service/eth/types.go
@@ -172,6 +172,7 @@ type (
 type ExecutionPayloadEnvelope struct {
 	ParentBeaconBlockRoot *common.Hash      `json:"parentBeaconBlockRoot,omitempty"`
 	ExecutionPayload      *ExecutionPayload `json:"executionPayload"`
+	RequestsHash          *common.Hash      `json:"requestsHash,omitempty"`
 }
 
 type ExecutionPayload struct {
