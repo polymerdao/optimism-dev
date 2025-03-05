5a8a8edf5a98569bd7535c66983490f3c0622e25
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2025-03-05 18:12:45 +0100

    fixup! fixup! fixup! fixup! update op-geth dependency to v1.101500.1

--- op-service/sources/types.go
+++ op-service/sources/types.go
@@ -131,6 +131,9 @@ type RPCHeader struct {
 	// ParentBeaconRoot was added by EIP-4788 and is ignored in legacy headers.
 	ParentBeaconRoot *common.Hash `json:"parentBeaconBlockRoot,omitempty"`
 
+	// RequestsHash was added by EIP-7685 and is ignored in legacy headers.
+	RequestsHash *common.Hash `json:"requestsHash,omitempty" rlp:"optional"`
+
 	// untrusted info included by RPC, may have to be checked
 	Hash common.Hash `json:"hash"`
 }
@@ -186,6 +189,8 @@ func (hdr *RPCHeader) createGethHeader() *types.Header {
 		BlobGasUsed:      (*uint64)(hdr.BlobGasUsed),
 		ExcessBlobGas:    (*uint64)(hdr.ExcessBlobGas),
 		ParentBeaconRoot: hdr.ParentBeaconRoot,
+		// Prague
+		RequestsHash: hdr.RequestsHash,
 	}
 }
 
@@ -315,6 +320,7 @@ func (block *RPCBlock) ExecutionPayloadEnvelope(trustCache bool) (*eth.Execution
 	return &eth.ExecutionPayloadEnvelope{
 		ParentBeaconBlockRoot: block.ParentBeaconRoot,
 		ExecutionPayload:      payload,
+		RequestsHash:          block.RequestsHash,
 	}, nil
 }
 
